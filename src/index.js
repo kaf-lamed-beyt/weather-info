81dceb3ef6986338403b11623ece292428b33f20
Caleb335 <belac335@gmail.com>
2020-04-15 20:03:56 +0100

    project setup complete

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Yo!!! Niggas let's see how this turns out.</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
